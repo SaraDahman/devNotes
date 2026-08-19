9957e82b84528c8d05167ac51897ab8005b2f69b
Sara Dahman <sara.dahman@ctrlagent.ai>
2026-08-19 22:30:21 +0300

    set up the database & experss server

---
+++ server/src/config/env.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+const env = {
+  port: Number(process.env.PORT) || 5000,
+
+  database: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+};
+
+export default env;
