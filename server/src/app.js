bd2a05a27979a3f7ec272420b138549de07d2505
Sara Dahman <sara.dahman@ctrlagent.ai>
2026-08-21 06:09:51 +0300

    fixed a bug

--- server/src/app.js
+++ server/src/app.js
@@ -19,7 +19,7 @@ if (env.nodeEnv === "production") {
   const clientDist = path.join(__dirname, "../../client/dist");
 
   app.use(express.static(clientDist));
-  app.get("*", (req, res) => {
+  app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(clientDist, "index.html"));
   });
 }
